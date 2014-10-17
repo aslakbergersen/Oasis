0d4431269799d025cc390d2eb8f1f043a9a956c4
Aslak Bergersen <aslak.bergersen@gmail.com>
2014-10-17 10:07:55 +0200

    Added stats, but missing flux and shear stress

--- problems/NSfracStep/Nozzle.py
+++ problems/NSfracStep/Nozzle.py
@@ -1,11 +1,13 @@
-from dolfin import *
+from problems import *
 from math import pi
-from fenicstools import StatisticsProbes    # StructuredGrid,
-from numpy import array, linspace           # sum
+from fenicstools import StatisticsProbes
+from numpy import array, linspace
+import sys
 
 # Override some problem specific parameters
 recursive_update(NS_parameters,
-                 dict(n=0.0035 / 1056.,
+                 dict(mu=0.0035,
+                      nu=0.0035 / 1056.,
                       T=1000,
                       dt=0.0001,
                       folder="nozzle_results",
@@ -17,8 +19,8 @@ recursive_update(NS_parameters,
                       velocity_degree=1,
                       mesh_path="mesh/8M_nozzle.xml",
                       print_intermediate_info=1000,
-                      use_lumping_of_mass_matrix=False,
-                      low_memory_version=False,
+                      use_lumping_of_mass_matrix=True,
+                      low_memory_version=True,
                       use_krylov_solvers=True,
                       krylov_solvers=dict(monitor_convergence=False,
                                           relative_tolerance=1e-8)))
@@ -46,7 +48,7 @@ def outlet(x, on_boundary):
 
 
 def create_bcs(V, sys_comp, **NS_namespce):
-    Q = 5.21E-6   # Re 6500: 6.77E-5  # From FDA
+    Q = 5.21E-6   # Re 6500: 6.77E-5 from FDA
     r_0 = 0.006
     u_maks = Q / (4*r_0*r_0*(1-2/pi))  # Analytical different r_0
     inn = Expression('u_maks * cos(sqrt(pow(x[0],2) + \
@@ -70,7 +72,7 @@ def initialize(q_, **NS_namespace):
     q_['u2'].vector()[:] = 1e-12
 
 
-def pre_solve_hook(velocity_degree, mesh, pressure_degree, V, **NS_namespace):
+def pre_solve_hook(velocity_degree, mesh, pressure_degree, V, **NS_namesepace):
     # To compute uv and flux
     Vv = VectorFunctionSpace(mesh, 'CG', velocity_degree,
                              constrained_domain=constrained_domain)
@@ -78,8 +80,8 @@ def pre_solve_hook(velocity_degree, mesh, pressure_degree, V, **NS_namespace):
                        constrained_domain=constrained_domain)
     normal = FacetNormal(mesh)
 
-    # No need to make the entire slice for validation.
-    # only need to avaluate in a line at this point
+    # No need to make the entire slice for validation
+    # only need to avaluate in a line at each z point.
     # And since the geometry is relativly small
     # (for now) it's ok to store the entire
     # figure for vizualisation.
@@ -88,18 +90,25 @@ def pre_solve_hook(velocity_degree, mesh, pressure_degree, V, **NS_namespace):
     # vectors = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
     # dL = [0.012, 0.012, 0]
     # N = [200, 200, 1]
-    # r3 = 0.008/0.022685 * 0.006 # On nozzle
-    # r1 = 0.006
-    # r2 = 0.002
+    r3 = 0.008/0.022685 * 0.006 # On nozzle
+    r1 = 0.006
+    r2 = 0.002
 
-    # z = [-0.18269, -0.088,-0.064, -0.048, -0.02, -0.008, 0.0, \
-    #         0.008, 0.016, 0.024, 0.032, 0.06, 0.08, 0.320]
-    # radius=[r1, r1, r1, r3, r2, r2, r2, r1, r1, r1, r1, r1, r1, r1]
-    # stats=[]
+    z = [-0.18269, -0.088,-0.064, -0.048, -0.02, -0.008, 0.0, \
+         0.008, 0.016, 0.024, 0.032, 0.06, 0.08, 0.320]
+    radius=[r1, r1, r1, r3, r2, r2, r2, r1, r1, r1, r1, r1, r1, r1]
 
-    # for z_ in z:
-    #     stats.append(StructuredGrid(V, N, [-0.006, -0.006, z_],
-    #                                  vectors, dL, statistics=True))
+    slices_u = []
+    slices_ss = []
+
+    for i in range(len(z)):
+        eval_points = array([[x, 0 z[i]] for x in linspace(-radius[i],
+                                                           radius[i], 1000)])
+        slices_u.append(StatisticsProbes(eval_points.flatten(), Vv))
+        slices_ss.append(StatisticsProbes(eval_points.flatten(), Vv))
+        #stats.append(StructuredGrid(V, N, [-0.006, -0.006, z_],
+        #                            vectors, dL, statistics=True))
+
 
     # Normals and domains to compute flux at at
     # each point (z), inlet and outlet
@@ -111,8 +120,11 @@ def pre_solve_hook(velocity_degree, mesh, pressure_degree, V, **NS_namespace):
     # mark domanis
     Inlet.mark(domains, 1)
     Outlet.mark(domains, 2)
+    Slice.mark(domains, 3)
 
-    # Evaluate the centerline and the wall
+    plot(Inlet, interactive=True)
+
+    # Setup probes in the centerline and at the wall
     z_senterline = linspace(-0.18269, 0.32, 10000)
     x_senter = array([[0.0, 0.0, i] for i in z_senterline])
     x_wall = []
@@ -136,22 +148,35 @@ def pre_solve_hook(velocity_degree, mesh, pressure_degree, V, **NS_namespace):
         x_wall.append([0.0, r, z_])
 
     x_wall = array(x_wall)
-    senterline = StatisticsProbes(x_senter.flatten(), V)
-    wall = StatisticsProbes(x_wall.flatten(), V)
+    senterline_u = StatisticsProbes(x_senter.flatten(), Vv)
+    senterline_p = StatisticsProbes(x_senter.flatten(), Pv)
+    senterline_ss = StatisticsProbes(x_senter.flatten(), Pv)
+    wall_p = StatisticsProbes(x_wall.flatten(), Pv)
+    wall_wss = StatisticsProbes(x_wall.flatten(), Pv)
 
-    return dict(uv=Function(Vv), pv=Function(Pv), radius=radius,
-                u_diff=Function(Vv), u_prev=Function(Vv), Vv=Vv,
-                wall=wall, senterline=senterline, domains=domains,
-                Pv=Pv, z_senterline=z_senterline, normal=normal, stats=stats)
+    #uv = Function(Vv)
+    #pv = Function(Pv)
+
+    #def epsilon(u):
+    #    return 
+
+    #sigma = 2*nu * epsilon(uv)
+
+    return dict(uv=Function(Vv), pv=Function(Pv), ssv=Functon(Pv), 
+                radius=radius, u_diff=Function(Vv), u_prev=Function(Vv), 
+                Vv=Vv, wall_p=wall_p, wall_wss=wall_wss, senterline_u=senterline_u,
+                senterline_p=senterline_p, senterline_ss=senterline_ss, 
+                domains=domains, Pv=Pv, z_senterline=z_senterline, 
+                normal=normal, slices_u=slice_u, slices_ss=slice_ss, z=z)
 
 
 def temporal_hook(tstep, info_red, steady, dt, radius, u_diff, pv,
                   Pv, u_prev, u_, check_steady, domains, normal,
-                  Vv, uv, newfolder, mesh_resolution, mesh, p_, case,
-                  z_senterline, wall, folder, senterline, stats,
-                  **NS_namespace):
+                  Vv, uv, newfolder, mesh, p_, case, wall_p, wall_wss,
+                  z_senterline, folder, senterline_u, senterline_p,
+                  senterline_ss, slices_u, slices_ss, z, **NS_namespace):
 
-    # steady state
+    # Check steady state
     if tstep % check_steady == 0:
         # uv.assign(project(u_, Vv))
         # file = File(newfolder + "/VTK/nozzle_velocity_%0.2e_%0.2e_%d.pvd" \
@@ -174,26 +199,32 @@ def temporal_hook(tstep, info_red, steady, dt, radius, u_diff, pv,
             info_red("Flux in: %e\nFlux out: %e\nRelativ error: %e\ntstep: %d"
                      % (inlet_flux, outlet_flux, rel_err, tstep))
 
-        if diff < 0.5:  # 2.5e-4:
-            steady = True   # Dave stats and kill the program
+        if diff < 2.5e-4:
+            steady = True   # Save stats and kill the program
         else:
             u_prev.assign(uv)
 
     if steady:
-        print 'OK'
-        # save a plot
+        # Variables to store
+        #ssv.assign(project(tau(u), Pv))
         uv.assign(project(u_, Vv))
         pv.assign(project(p_, Pv))
-        print 'OK1'
-        senterline(uv)
-        print 'OK2'
-        wall(pv)
-        print 'OK3'
-        for i in range(len(stats)):
-            stats[i](uv)
-        print 'OK4'
 
-    if senterline.number_of_evaluations == 100:
+        # Evaluate senterline
+        senterline_u(uv)
+        senterline_p(pv)
+        #senterline_ss(ssv)
+
+        # Evaluate at the wall
+        wall_p(pv)
+        #wall_wss(ssv)
+
+        # Evaluate for each slice
+        for i in range(len(slices_u)):
+            slices_u[i](uv)
+            #slices_ss[i](ssv)
+
+    if senterline_u.number_of_evaluations == 100:
         file = File(newfolder + "/VTK/nozzle_velocity_%0.2e_%0.2e.pvd"
                     % (dt, mesh.hmin()))
         file << uv
@@ -204,29 +235,31 @@ def temporal_hook(tstep, info_red, steady, dt, radius, u_diff, pv,
         # save the data from the centerline
         info = open(newfolder + "/Stats/nozzle_stats.txt", 'w')
         info = write_overhead(info, dt, tstep*dt, mesh.hmin(),
-                              case, mesh_resolution)
-        info.write("Axial velocity\n")
-        info.write("   z            value\n")
+                              mesh.hmax())
+        
+        senterline_u(uv)
+        info = write_data(info, senterline_u, "Velocity senterline")
 
-        senterline(uv)
-        data = senterline.array()
-        for i in range(len(data)):
-            info.write("%f %s\n" % (z_senterline[i], data[i]))
+        senterline_ss(ssv)
+        info = write_data(info, senterline_ss, "Share stress senterline")
 
-        # save information for each slice
-        for i in range(len(stats)):
-            stats[i](uv)
-            stats[i].toh5(0, tstep, filename=newfolder+"/VTK/slice_z_%s.h5"
-                          % (radius[i]))
+        senterline_p(pv)
+        info = write_data(info, senterline_p, "Pressure senterline")
 
-        # save preassure
-        info.write("Wall pressure\n")
+        wall_p(pv)
+        info = write_data(info, wall_p, "Wall pressure")
 
-        wall(pv0)
-        data = wall.array()
-        info.write("   z            value\n")
-        for i in range(len(data)):
-            info.write("%f %s\n" % (z_senterline[i], data[i]))
+        wall_wss(ssv)
+        info = write_data(info, wall_wss, "Wall share stress")
+
+        for slice in range(len(slices_u)):
+            slice[i](uv)
+            info = write_data(info, slice, "Axial velocity at z=%d" % z[i])
+
+        for slice in range(len(slices_u)):
+            slice[i](uv)
+            info = write_data(info, slice, "Axial share stress at z=%d" % z[i])
+
 
         info.close
         kill = open(folder + '/killoasis', 'w')
@@ -234,11 +267,20 @@ def temporal_hook(tstep, info_red, steady, dt, radius, u_diff, pv,
         # TODO: create a file that is called killoasis?
 
 
-def write_overhead(File, dt, T, hmin, Re, mesh_resolution):
+def write_overhead(File, dt, T, hmin, Re, hmax):
     File.write("===Nozzle with sudden expanssion===\n")
     File.write("dt=%e\n" % dt)
     File.write("stopp time=%f\n" % T)
     File.write("hmin=%e\n" % hmin)
+    File.write("hmax=%s\n" % hmax)
     File.write("Re=%d\n" % Re)
-    File.write("Mesh=%s\n" % mesh_resolution)
+    return File
+
+def write_data(File, probes, headline, direction=2):
+    array = probes.array()
+    points = probes.coordinates()
+    File.write(headline)
+    File.write("%d\n" % len(array))
+    for i in range(len(array)):
+        File.write("%e %e\n" % points[direction][i], array[i]
     return File
