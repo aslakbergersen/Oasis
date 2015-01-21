8e61e6e2ad713075e83a6699ae944b5ba455ed9a
Aslak Bergersen <aslak.bergersen@gmail.com>
2015-01-21 15:36:24 +0100

    Add test script

--- test_HDF5.py
+++ test_HDF5.py
@@ -1,6 +1,7 @@
 from dolfin import *
+from numpy import *
 
 newfile = HDF5File(mpi_comm_world(), "test.h5", "w")
 newfile.flush()
-newfile.write(4, "/current")
+newfile.write(array([4,5]), "/current")
 newfile.close()
