c890cb5d8b4bc15622315e5d52a4410b1f07c36c
Aslak Bergersen <aslak.bergersen@gmail.com>
2018-09-13 09:57:53 +0200

    Change Max to max_values

--- oasis/solvers/NSfracStep/LES/Wale.py
+++ oasis/solvers/NSfracStep/LES/Wale.py
@@ -3,8 +3,9 @@ __date__ = '2015-01-22'
 __copyright__ = 'Copyright (C) 2015 ' + __author__
 __license__ = 'GNU Lesser GPL version 3 or any later version'
 
+from ufl import max_value
 from dolfin import (Function, FunctionSpace, assemble, TestFunction, sym, grad, tr,
-    Identity, dx, inner, Max, MeshFunction, DirichletBC, Constant)
+    Identity, dx, inner, MeshFunction, DirichletBC, Constant)
 
 from .common import derived_bcs
 
@@ -28,7 +29,7 @@ def les_setup(u_, mesh, Wale, bcs, CG1Function, nut_krylov_solver, **NS_namespac
     dim = mesh.geometry().dim()
     Sd = sym(Gij * Gij) - 1. / 3. * Identity(mesh.geometry().dim()) * Skk * Skk
     nut_form = (Wale['Cw']**2 * pow(delta, 2. / dim) * pow(inner(Sd, Sd), 1.5)
-                / (Max(pow(inner(Sij, Sij), 2.5) + pow(inner(Sd, Sd), 1.25), 1e-6)))
+                / (max_value(pow(inner(Sij, Sij), 2.5) + pow(inner(Sd, Sd), 1.25), 1e-6)))
     ff = MeshFunction("size_t", mesh, 0)
     bcs_nut = derived_bcs(CG1, bcs['u0'], u_)
     nut_ = CG1Function(nut_form, mesh, method=nut_krylov_solver,
